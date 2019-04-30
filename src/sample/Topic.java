96232493697cc3580d792f1340e3695793c2d62e
zachchanak <47953007+zachchanak@users.noreply.github.com>
2019-04-29 21:55:13 -0500

    Add files via upload
    Topic class

---
+++ src/sample/Topic.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Topic {
+    private String topicName;
+    private ArrayList<Question> questions;
+
+    public Topic(String topic) {
+        topicName = topic;
+        questions = new ArrayList<Question>();
+    }
+
+    private void addQuestion(Question question) {
+        questions.add(question);
+    }
+    private ArrayList<Question> getQuestions(int numQuestions) {
+        if(numQuestions<questions.size()) {
+            ArrayList<Question> duplicate = new ArrayList<Question>();
+            ArrayList<Question> returnList = new ArrayList<Question>();
+            //Create the duplicate
+            for (int i=0; i < questions.size(); i ++) {
+                duplicate.add(questions.get(i));
+            }
+            int remainingQuestions = numQuestions;
+            Random rand = new Random();
+            //Get some questions
+            for (int i=0; i <numQuestions; i++) {
+                int index = rand.nextInt(remainingQuestions);
+                returnList.add(duplicate.get(index));
+                duplicate.remove(index);
+                remainingQuestions--;
+                
+            }
+            return returnList;
+        }
+        //They want all of the topic questions
+        else {
+            return questions;
+        }
+    }
+
+
+}
