791722376612dda7a1db5a7d6b870c8d15142c7a
Dylan Clark <36828309+daclark19@users.noreply.github.com>
2019-05-03 15:44:52 -0500

    Commented the Question Class
    Added comments and file header

--- src/sample/Question.java
+++ src/sample/Question.java
@@ -1,16 +1,64 @@
-package sample;
 
+//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
+//
+// Title:           Question
+// Files:           (Quiz,JsonExport,Main,JsonParser,Quiz,Topic)
+// Course:          (CS 400, 2019)
+//
+// Author:          (Dylan Clark)
+// Email:           (daclark@wisc.edu)
+// Lecturer's Name: (Andrew Kuemmel)
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
+//
+// Partner Name:    (See README)
+// Partner Email:   (See README)
+// Partner Lecturer's Name: (See README)
+//
+// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
+//    N/A  Write-up states that pair programming is allowed for this assignment.
+//    N/A  We have both read and understand the course Pair Programming Policy.
+//    N/A  We have registered our team prior to the team registration deadline.
+//
+///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
+//
+// Students who get help from sources other than their partner must fully
+// acknowledge and credit those sources of help here.  Instructors and TAs do
+// not need to be credited here, but tutors, friends, relatives, room mates,
+// strangers, and others do.  If you received no outside help from either type
+//  of source, then please explicitly indicate NONE.
+//
+// Persons:         N/A
+// Previous Course: N/A
+// Online Sources:  N/A
+//
+/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
 import java.util.ArrayList;
 
+/**
+ * Question class that serves as the class that stores all of the fields of the question
+ * these questions are added to the Topic class and the Quiz class.
+ * @author Dylan Clark
+ */
 public class Question {
     // Field for the QuestionNode
-    String metadata;
-    String questionText;
-    String topic;
-    String imageName;
-    String correctAnswer;
-    ArrayList<String> allAnswers;
+    String metadata; //metadata field not really used
+    String questionText; //Contains the text for the question
+    String topic; //Contains the topic for the question
+    String imageName; //Name of the image if used
+    String correctAnswer; //The correct answer as a string
+    ArrayList<String> allAnswers; //All of the available answers
 
+    /**
+     * Constructor for the Question class that creates a new question
+     * with all of the required fields
+     * @param metadata not really used
+     * @param questionText the test of the question
+     * @param topic the topic of the question
+     * @param imageName the name of the image
+     * @param correctAnswer the string of the correct answer
+     * @param allAnswers the ArrayList of all the answers
+     */
     public Question(String metadata, String questionText, String topic, String imageName,
         String correctAnswer, ArrayList<String> allAnswers) {
         this.metadata = metadata;
@@ -21,26 +69,46 @@ public class Question {
         this.allAnswers = allAnswers;
     }
 
+    //Getter methods used for testing
+    /**
+     * Getter method for metadata
+     * @return current metadata
+     */
     public String getMetadata() {
         return this.metadata;
     }
-
+    /**
+     * Getter method for questionText
+     * @return current questionText
+     */
     public String questionText() {
         return this.questionText;
     }
-
+    /**
+     * Getter method for topic
+     * @return current topic
+     */
     public String getTopic() {
         return this.topic;
     }
-
+    /**
+     * Getter method for imageName
+     * @return current image name
+     */
     public String imageName() {
         return this.imageName;
     }
-
+    /**
+     * Getter method for correctAnswer
+     * @return current correct answer
+     */
     public String getCorrectAnswer() {
         return this.correctAnswer;
     }
-
+    /**
+     * Getter method for allAnswers
+     * @return current list of all answers
+     */
     public ArrayList<String> getallAnswers() {
         return this.allAnswers;
     }
